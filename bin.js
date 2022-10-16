34a243acfb46d4994d7bdb89dd76c99a15ebc680
Kyle E. Mitchell <kyle@kemitchell.com>
2022-10-16 12:40:38 -0700

    Initial Commit

---
+++ bin.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const process = require('process')
+
+// If there are arguments, process them.
+const argumentInputs = process.argv.slice(2)
+if (argumentInputs.length) transform(argumentInputs)
+
+// Otherwise, process standard input.
+const chunks = []
+process.stdin
+  .on('data', chunk => { chunks.push(chunk) })
+  .once('error', error => {
+    console.error(error)
+    process.exit(1)
+  })
+  .once('end', () => {
+    transform(
+      Buffer.concat(chunks)
+        .toString('utf8')
+        // Remove any trailing newline.
+        .replace(/\n\r?$/, '')
+        // Split into lines.
+        .split(/\n\r?/)
+    )
+  })
+
+function transform (inputs) {
+  const { titleCase } = require('title-case')
+  for (const input of inputs) {
+    process.stdout.write(titleCase(input) + '\n')
+  }
+  process.exit(0)
+}
